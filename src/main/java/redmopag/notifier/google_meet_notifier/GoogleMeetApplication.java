05244d055cbe325f65e00b539965bd30d8b67cd0
redmopag <redmopag@gmail.com>
2024-12-09 19:28:34 +0500

    Add the code to create a subscription to the participant's join event in the meet space

--- src/main/java/redmopag/notifier/google_meet_notifier/GoogleMeetApplication.java
+++ src/main/java/redmopag/notifier/google_meet_notifier/GoogleMeetApplication.java
@@ -1,7 +1,9 @@
 package redmopag.notifier.google_meet_notifier;
 
 import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.apps.events.subscriptions.v1.*;
 import com.google.auth.Credentials;
+import com.google.protobuf.Duration;
 import redmopag.notifier.google_meet_notifier.utils.CredentialsSettings;
 import com.google.apps.meet.v2.CreateSpaceRequest;
 import com.google.apps.meet.v2.Space;
@@ -13,20 +15,37 @@ import java.io.IOException;
 public class GoogleMeetApplication {
     private static final CredentialsSettings CREDENTIALS_SETTINGS = new CredentialsSettings();
 
+    private static String targetResource = "//meet.google.com/ajz-ajjo-uxw";
+    private static String topicName = "projects/meet-notifier-443718/topics/meet-events-topic";
+
     public static void main(String[] args) throws Exception {
+        // Настройка доступа
         Credentials credentials = CREDENTIALS_SETTINGS.getCredentials();
-        SpacesServiceSettings settings = SpacesServiceSettings.newBuilder()
+        SubscriptionsServiceSettings settings = SubscriptionsServiceSettings.newBuilder()
                 .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
                 .build();
 
-        try (SpacesServiceClient spacesServiceClient = SpacesServiceClient.create(settings)) {
-            CreateSpaceRequest request = CreateSpaceRequest.newBuilder()
-                    .setSpace(Space.newBuilder().build())
+        Subscription subscription = Subscription.newBuilder()
+                .setTargetResource(targetResource) // настройка отслеживаемого ресурса
+                .addEventTypes("google.workspace.meet.participant.v2.joined") // тип события
+                // будут приходить в topic Pub/Sub
+                .setNotificationEndpoint(NotificationEndpoint.newBuilder()
+                        .setPubsubTopic(topicName)
+                        .build())
+                .setPayloadOptions(PayloadOptions.newBuilder()
+                        .setIncludeResource(false)
+                        .build())
+                .setTtl(Duration.newBuilder()
+                        .setSeconds(86400)
+                        .build())
+                .build();
+
+        try (SubscriptionsServiceClient subscriptionsServiceClient = SubscriptionsServiceClient.create(settings)) {
+            CreateSubscriptionRequest request = CreateSubscriptionRequest.newBuilder()
+                    .setSubscription(subscription)
                     .build();
-            Space response = spacesServiceClient.createSpace(request);
-            System.out.println("Space created: " + response.getMeetingUri());
-        } catch (IOException e){
-            e.printStackTrace();
+
+            Subscription response = subscriptionsServiceClient.createSubscriptionAsync(request).get();
         }
     }
 }
